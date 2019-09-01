8646379ef1fffea881402b06f008ab4b846aabd0
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2019-09-01 17:43:13 +0200

    Add simple navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
 
+enum Page {
+  Recipes,
+  ShoppingList,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  private currentPage = Page.Recipes;
+
+  setPageToRecipes() {
+    this.currentPage = Page.Recipes;
+  }
+
+  setPageToShoppingList() {
+    this.currentPage = Page.ShoppingList;
+  }
+
+  doShowRecipesPage(): boolean {
+    return this.currentPage == Page.Recipes;
+  }
+
+  doShowShoppingListPage(): boolean {
+    return this.currentPage == Page.ShoppingList;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,8 +6,18 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() private recipesClicked = new EventEmitter();
+  @Output() private shoppingListClicked = new EventEmitter();
 
   toggleCollapse() {
     this.collapsed = !this.collapsed;
+  }
+
+  onRecipesClicked() {
+    this.recipesClicked.emit();
+  }
+
+  onShoppingListClicked() {
+    this.shoppingListClicked.emit();
   }
 }
